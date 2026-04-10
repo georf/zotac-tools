66cb569e81a4918ba7cf7812d75161b164b4ba44
Georg Limbach <georg.limbach@lichtbit.com>
2026-04-10 14:17:01 +0200

    Leistung pro PTPP übergeben

--- deye12k.py
+++ deye12k.py
@@ -21,7 +21,7 @@ MQTT_PORT = int(os.getenv("MQTT_PORT", 1883))
 MQTT_USER = os.getenv("MQTT_USER")
 MQTT_PASSWORD = os.getenv("MQTT_PASS")
 
-POLL_INTERVAL = 600  # 10 Minuten
+POLL_INTERVAL = 60  # 1 Minute
 
 # ======================
 # MQTT
@@ -39,26 +39,76 @@ inv = PySolarmanV5(INVERTER_IP, INVERTER_SERIAL)
 # Registers
 # ======================
 REGISTERS = {
-    "pv_today": HoldingRegisters.TodayFromPV,
-    "pv_total": HoldingRegisters.TotalFromPV,
-    "battery_charge_today": HoldingRegisters.BatteryChargeToday,
-    "battery_charge_total": HoldingRegisters.BatteryChargeTotal,
-    "battery_discharge_today": HoldingRegisters.BatteryDischargeToday,
-    "battery_discharge_total": HoldingRegisters.BatteryDischargeTotal,
+    "pv_today": {
+        "register": HoldingRegisters.TodayFromPV,
+        "unit": "kWh",
+        "device_class": "energy",
+        "state_class": "total_increasing",
+    },
+    "pv_total": {
+        "register": HoldingRegisters.TotalFromPV,
+        "unit": "kWh",
+        "device_class": "energy",
+        "state_class": "total_increasing",
+    },
+    "battery_charge_today": {
+        "register": HoldingRegisters.BatteryChargeToday,
+        "unit": "kWh",
+        "device_class": "energy",
+        "state_class": "total_increasing",
+    },
+    "battery_charge_total": {
+        "register": HoldingRegisters.BatteryChargeTotal,
+        "unit": "kWh",
+        "device_class": "energy",
+        "state_class": "total_increasing",
+    },
+    "battery_discharge_today": {
+        "register": HoldingRegisters.BatteryDischargeToday,
+        "unit": "kWh",
+        "device_class": "energy",
+        "state_class": "total_increasing",
+    },
+    "battery_discharge_total": {
+        "register": HoldingRegisters.BatteryDischargeTotal,
+        "unit": "kWh",
+        "device_class": "energy",
+        "state_class": "total_increasing",
+    },
+    "pv1_power": {
+        "register": HoldingRegisters.PV1InPower,
+        "unit": "W",
+        "device_class": "power",
+        "state_class": "measurement",
+        "last_value": None,
+    },
+    "pv2_power": {
+        "register": HoldingRegisters.PV2InPower,
+        "unit": "W",
+        "device_class": "power",
+        "state_class": "measurement",
+        "last_value": None,
+    },
+    "pv_power": {
+        "sum": ["pv1_power", "pv2_power"],
+        "unit": "W",
+        "device_class": "power",
+        "state_class": "measurement",
+    },
 }
 
 # ======================
 # Home Assistant Discovery
 # ======================
 def publish_discovery():
-    for key in REGISTERS:
+    for key, cfg in REGISTERS.items():
         payload = {
             "name": f"Deye12K {key}",
-            "device_class": "energy",
+            "device_class": cfg["device_class"],
             "state_topic": f"deye12k/{key}/state",
-            "unit_of_measurement": "kWh",
+            "unit_of_measurement": cfg["unit"],
             "unique_id": f"deye12k_{key}",
-            "state_class": "total_increasing",
+            "state_class": cfg["state_class"],
             "device": {
               "identifiers": [
                 "deye12k"
@@ -89,8 +139,16 @@ def main():
 
     while True:
         inv = PySolarmanV5(INVERTER_IP, INVERTER_SERIAL)
-        for key, register in REGISTERS.items():
-            value = read_register(register)
+        for key, cfg in REGISTERS.items():
+            value = None
+            if "sum" in cfg:
+                if any(REGISTERS[sub_key]["last_value"] is None for sub_key in cfg["sum"]):
+                    continue
+                value = sum(REGISTERS[sub_key]["last_value"] for sub_key in cfg["sum"])
+                cfg["last_value"] = value
+            else:
+              value = read_register(cfg["register"])
+              cfg["last_value"] = value
             
             # Debug output
             print(f"Publishing {key} to topic deye12k/{key}/state with value: {value}")
